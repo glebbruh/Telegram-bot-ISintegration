85e9ea614d05b02193f5b97725a63932b5d394b5
Gleb Poletaev <poletaev.gl@gmail.com>
2026-05-10 21:14:23 +0300

    update

--- app/api/app/main.py
+++ app/api/app/main.py
@@ -4,6 +4,7 @@ from app.routers.auth import router as auth_router
 from app.routers.inspections import router as inspections_router
 from app.routers.patterns import router as patterns_router
 from app.routers.tasks import router as tasks_router
+from app.routers.webhooks import router as webhooks_router
 
 app = FastAPI(title="CheckOffice API")
 
@@ -11,6 +12,7 @@ app.include_router(auth_router)
 app.include_router(inspections_router)
 app.include_router(patterns_router)
 app.include_router(tasks_router)
+app.include_router(webhooks_router)
 
 
 @app.get("/health")
